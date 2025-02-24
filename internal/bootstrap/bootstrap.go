ab85a41cbcc0d76a805ccee83fc2c3c420b0d0b8
meedeley <nicholasaputra77@gmail.com>
2025-02-25 02:36:19 +0700

    add : implement dig as depedency injection

---
+++ internal/bootstrap/bootstrap.go
@@ -0,0 +1,43 @@
+package bootstrap
+
+import (
+	"github.com/meedeley/go-launch-starter-code/internal/conf"
+	"github.com/meedeley/go-launch-starter-code/internal/delivery/http/handlers"
+	"github.com/meedeley/go-launch-starter-code/internal/delivery/http/middlewares"
+	"github.com/meedeley/go-launch-starter-code/internal/usecase"
+	"go.uber.org/dig"
+)
+
+func registerApp(container *dig.Container) {
+	container.Provide(conf.ProvideApp)
+	container.Provide(conf.NewPool)
+}
+
+func registerMiddleware(container *dig.Container) {
+	container.Provide(middlewares.NewAuthMiddleware().GuestOnly)
+	container.Provide(middlewares.NewAuthMiddleware().Protected)
+}
+
+func registerHandler(container *dig.Container) {
+	container.Provide(handlers.NewUserHandler)
+}
+
+func registerUseCase(container *dig.Container) {
+	container.Provide(usecase.NewUserUseCase)
+}
+
+func registerRespository(container *dig.Container) {
+	// container.Provide()
+}
+
+func BuildContainer() *dig.Container {
+	container := dig.New()
+
+	registerApp(container)
+	registerRespository(container)
+	registerUseCase(container)
+	registerHandler(container)
+	registerMiddleware(container)
+
+	return container
+}
